de76ff6f7029d432bf0a5d33d5e656f5084bdbcb
Максим Миндавлетов <maksimmindavletov@Mac.ufanet.ru>
2024-11-05 05:20:33 +0500

    number 3

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,12 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+    public static String getName() {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.next();
+        scanner.close();
+        return name;
+    }
+}
